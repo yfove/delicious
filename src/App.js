1a932cbb31c2faaf33d8380bc97bfb546b259a45
curtis chat <curtis.ch.chat@gmail.com>
2022-04-21 16:45:08 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Pages from "./pages/Pages";
+import Category from './components/Category'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello</h1>
+     <Pages />
+
+     fetch('spoontacular.com/api&key="process.env."')
+    </div>
+  );
+}
+
+export default App;
